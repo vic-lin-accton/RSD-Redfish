ea2ae6af3d81f831531eb7b9ab739d190754d6a9
nick_huang <abrother94ec@gmail.com>
2019-08-05 07:38:30 +0000

    1.Add unitest for GPON 64 ports 2 40G NNI traffic OK.

--- PSME/tools/deb_maker/install/allinone-deb/sh/prepare.sh
+++ PSME/tools/deb_maker/install/allinone-deb/sh/prepare.sh
@@ -121,6 +121,7 @@ else
     cp ${PSME_ACC_SW_DIR}/onu_cfg                         $ITEM_PATH/usr/local/bin
     cp ${PSME_ACC_SW_DIR}/onu_cfg                         $ITEM_PATH/broadcom
     cp ${PSME_ACC_SW_DIR}/xgspon_unit_test_onu_cfg        $ITEM_PATH/broadcom
+    cp ${PSME_ACC_SW_DIR}/gpon_unit_test_onu_cfg          $ITEM_PATH/broadcom
     cp $LIB_PREINSTALL/libossp-uuid.so.16                 $ITEM_PATH/usr/local/lib
     cp $LIB_PREINSTALL/libossp-uuid++.so.16               $ITEM_PATH/usr/local/lib
 fi
